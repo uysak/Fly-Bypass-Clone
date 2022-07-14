445f84c4cc5c786378339f4193c6717d257e5c90
Berkay Uysak <77589774+uysak@users.noreply.github.com>
2022-07-14 20:36:29 +0300

    Added: Interfaces

---
+++ Assets/Scripts/Interfaces/Interfaces.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interfaces : MonoBehaviour
+{
+    public interface ICollectible
+    {
+        void Collect();
+    }
+    public interface IDestroyable
+    {
+        void Destroy();
+    }
+}
